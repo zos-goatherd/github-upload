57bd0592ff0868fcdaab942434d92d0f76ed2d82
zos <zos.the.goatherd@gmail.com>
2021-04-28 11:35:15 -0400

    complete chapter2 assignments

---
+++ assignments/chapter2/exercise_2-3.py
@@ -0,0 +1,7 @@
+# Assignment 2.3 from Python for Everybody
+# simple program to calculate total pay by multiplying input hours worked and
+# hourly rate
+hoursWorked = input("Enter Hours: ")
+payRate = input("Enter Rate: ")
+totalPay = float(hoursWorked) * float(payRate)
+print("Pay", totalPay)
