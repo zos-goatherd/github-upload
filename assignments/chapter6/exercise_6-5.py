76df6d4bf5317946bae55ef30722680f7c2b6e83
zos <zos.the.goatherd@gmail.com>
2021-06-21 13:01:29 -0400

    add chapter 6 assignments
    add exercise_6-5.py the assignment for chapter 6 that was submitted to
    the autograder

---
+++ assignments/chapter6/exercise_6-5.py
@@ -0,0 +1,10 @@
+#!/usr/bin python3
+
+
+# Use find and string slicing to extract the portion of the string after
+# the colon and convert it to a floating point value
+str = 'X-DSPAM-Confidence:0.8475'
+delim = str.find(':')
+numstr = str[delim+1:]
+num = float(numstr)
+print(num)
