92137e17c5e22240783535ced44ff511bf36ac66
zos <zos.the.goatherd@gmail.com>
2021-06-23 16:26:15 -0400

    add chapter 7 exercies
    completed all 3 chapter 7 exercises that involved reading from a file

--- assignments/chapter6/exercise_6-5.py
+++ assignments/chapter6/exercise_6-5.py
@@ -1,8 +1,7 @@
-#!/usr/bin python3
+#!/usr/bin/python3
+''' Use find and string slicing to extract the portion of the string after
+ the colon and convert it to a floating point value'''
 
-
-# Use find and string slicing to extract the portion of the string after
-# the colon and convert it to a floating point value
 str = 'X-DSPAM-Confidence:0.8475'
 delim = str.find(':')
 numstr = str[delim+1:]
